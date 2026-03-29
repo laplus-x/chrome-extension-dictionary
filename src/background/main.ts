2ebb05f223cd7897406151b12825f7fcf6331160
陳思瑜 Judy <laplus.space@gmail.com>
2026-03-29 21:25:19 +0800

    feat(src/background): 初始化

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,6 +1,13 @@
 chrome.runtime.onMessage.addListener(async (msg, sender) => {
-  if (msg.type === "OPEN_SIDE_PANEL" && sender.tab?.windowId) {
+  if (!sender.tab?.windowId) return;
+
+  if (msg.type === "OPEN_SIDE_PANEL") {
     await chrome.sidePanel.open({
+      windowId: sender.tab.windowId,
+    });
+  }
+  if (msg.type === "CLOSE_SIDE_PANEL") {
+    await chrome.sidePanel.close({
       windowId: sender.tab.windowId,
     });
   }
